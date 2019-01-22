826d53bf747b9489e986d1653afc92cdae3c18e4
Ricardo Mayo <rmayo.1995@gmail.com>
2019-01-22 14:54:47 -0600

    added front build

---
+++ public/precache-manifest.f158f057cd848c2bfde6afe98a0fd9c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "888773434c0deab1c0b8",
+    "url": "/static/js/main.88877343.chunk.js"
+  },
+  {
+    "revision": "fdbf855fb5277779f931",
+    "url": "/static/js/1.fdbf855f.chunk.js"
+  },
+  {
+    "revision": "888773434c0deab1c0b8",
+    "url": "/static/css/main.1eb29ea1.chunk.css"
+  },
+  {
+    "revision": "fdbf855fb5277779f931",
+    "url": "/static/css/1.dd301361.chunk.css"
+  },
+  {
+    "revision": "61efd61a514587d6ecd35b02d33bca7c",
+    "url": "/index.html"
+  }
+];
